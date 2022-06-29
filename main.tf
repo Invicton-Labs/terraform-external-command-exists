77cb5961100d3bf3a376ba8b504ff59b3cfeac2c
Kyle Kotowick <kotowick@invictonlabs.com>
2022-06-29 15:35:18 -0400

    Update internal module versions; add testing (#1)

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-terraform {
-  required_version = ">= 0.13.0"
-}
-
 locals {
   is_windows      = dirname("/") == "\\"
-  command_windows = var.command_windows != null ? var.command_windows : var.command_unix
-  command_unix    = var.command_unix != null ? var.command_unix : var.command_windows
+  command_windows = local.var_command_windows != null ? local.var_command_windows : local.var_command_unix
+  command_unix    = local.var_command_unix != null ? local.var_command_unix : local.var_command_windows
   command         = local.is_windows ? local.command_windows : local.command_unix
   stdout          = module.command_exists.stdout != "" ? module.command_exists.stdout : (local.is_windows ? "True" : "0")
   exists          = module.command_exists.stdout != "" ? local.is_windows ? tobool(lower(module.command_exists.stdout)) : module.command_exists.stdout == "0" : null
@@ -14,18 +10,19 @@ locals {
 // Ensure that at least one command is provided
 module "assert_command_provided" {
   source        = "Invicton-Labs/assertion/null"
-  version       = "~> 0.2.1"
+  version       = "~>0.2.1"
   error_message = "At least one of the `command_unix` or `command_windows` input variable must be provided."
-  condition     = var.command_unix != null || var.command_windows != null
+  condition     = local.var_command_unix != null || local.var_command_windows != null
 }
 
 module "command_exists" {
-  source          = "Invicton-Labs/shell-data/external"
-  version         = "~> 0.2.1"
-  command_unix    = "command -v \"$COMMAND\" >/dev/null 2>&1; echo $?"
-  command_windows = "[bool](Get-Command -Name \"$Env:COMMAND\" -ErrorAction SilentlyContinue)"
-  working_dir     = var.working_dir != null ? var.working_dir : path.module
-  fail_on_error   = true
+  source                    = "Invicton-Labs/shell-data/external"
+  version                   = "~>0.4.0"
+  command_unix              = "command -v \"$COMMAND\" >/dev/null 2>&1; echo $?"
+  command_windows           = "[bool](Get-Command -Name \"$Env:COMMAND\" -ErrorAction SilentlyContinue)"
+  working_dir               = local.var_working_dir != null ? local.var_working_dir : path.module
+  fail_on_nonzero_exit_code = true
+  fail_on_stderr            = true
   environment = {
     COMMAND = local.command
   }
@@ -34,8 +31,8 @@ module "command_exists" {
 // Ensure that the command exists, in order to fail Terraform if desired
 module "asset_command_exists" {
   source        = "Invicton-Labs/assertion/null"
-  version       = "~> 0.2.1"
+  version       = "~>0.2.1"
   error_message = "The command \"${local.command}\" is not available in ${local.is_windows ? "PowerShell" : "the shell"}."
   // Only check the condition if we should fail if it's missing
-  condition = var.fail_if_command_missing ? (local.exists == null ? true : local.exists) : true
+  condition = local.var_fail_if_command_missing ? (local.exists == null ? true : local.exists) : true
 }
--- variables.tf
+++ variables.tf
@@ -2,12 +2,26 @@ variable "command_unix" {
   description = "The command to check for on unix-based systems. If not provided, the `command_windows` variable will be used."
   type        = string
   default     = null
+  validation {
+    condition     = var.command_unix == null ? true : length(replace(replace(replace(var.command_unix, "\r", ""), "\n", ""), "\r\n", "")) == length(var.command_unix)
+    error_message = "The `command_unix` input variable, if provided, may not contain any newline (\\n) or carriage return (\\r) characters."
+  }
+}
+locals {
+  var_command_unix = var.command_unix
 }
 
 variable "command_windows" {
   description = "The command to check for on Windows systems. If not provided, the `command_unix` variable will be used."
   type        = string
   default     = null
+  validation {
+    condition     = var.command_windows == null ? true : length(replace(replace(replace(var.command_windows, "\r", ""), "\n", ""), "\r\n", "")) == length(var.command_windows)
+    error_message = "The `command_windows` input variable, if provided, may not contain any newline (\\n) or carriage return (\\r) characters."
+  }
+}
+locals {
+  var_command_windows = var.command_windows
 }
 
 variable "fail_if_command_missing" {
@@ -15,9 +29,15 @@ variable "fail_if_command_missing" {
   type        = bool
   default     = false
 }
+locals {
+  var_fail_if_command_missing = var.fail_if_command_missing != null ? var.fail_if_command_missing : false
+}
 
 variable "working_dir" {
   description = "The directory to check for the command in. Defaults to the module directory."
   type        = string
   default     = null
+}
+locals {
+  var_working_dir = var.working_dir
 }
